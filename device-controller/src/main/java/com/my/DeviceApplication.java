923d1cad93b0a3fb9d12fd9679677d7905e86203
Look <736116531@qq.com>
2019-08-12 21:08:43 +0800

    add dependecy for project

--- device-controller/src/main/java/com/my/DeviceApplication.java
+++ device-controller/src/main/java/com/my/DeviceApplication.java
@@ -2,9 +2,18 @@ package com.my;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeviceApplication {
+public class DeviceApplication  extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // TODO Auto-generated method stub
+        return builder.sources(DeviceApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DeviceApplication.class, args);
 	}
